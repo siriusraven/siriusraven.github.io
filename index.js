eebc5287069dad76983161f8cafd8b0986bb4bcf
siriusraven <csongorkrisztiansete9@gmail.com>
2024-09-17 18:11:43 +0200

    added socials
    social media links with placeholder modals added and README changed

---
+++ index.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("messageModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("messageModal").style.display = "none";
+}
